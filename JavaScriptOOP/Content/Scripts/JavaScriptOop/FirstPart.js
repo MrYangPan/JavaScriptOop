5f0c7db2d656eabb42ba3979a50af9163c488438
yangpan <123456>
2016-07-08 09:48:05 +0800

    oop代码整理

--- JavaScriptOOP/Content/Scripts/JavaScriptOop/FirstPart.js
+++ JavaScriptOOP/Content/Scripts/JavaScriptOop/FirstPart.js
@@ -1090,7 +1090,7 @@ var javaScriptOop = function () {
                     //我们还可以用循环语句，来批量注册这些 isType 函数： 
                     var reconstruct2 = function () {
                         var Type = {};
-                        for (var i = 0, type; type = ['String', 'Array', 'Number'][i++];) {
+                        for (var i = 0, type; type === ['String', 'Array', 'Number'][i++];) {
                             (function (type) {
                                 Type['is' + type] = function (obj) {
                                     return Object.prototype.toString.call(obj) === '[object ' + type + ']';
@@ -1105,26 +1105,56 @@ var javaScriptOop = function () {
                 //2.单例模式
                 var getSingle = function () {
 
-                    var getSingle = function(fn) {
+                    var getSingle = function (fn) {
                         var ret;
-                        return function() {
-                             return ret || (ret = fn.apply(this, arguments));
+                        return function () {
+                            return ret || (ret = fn.apply(this, arguments));
                         };
                     };
-
-
-
                 }
-
-
             }
 
+            //高阶函数实现AOP 
+            var aopFunction = function () {
 
+                /*
+                AOP（面向切面编程）的主要作用是把一些跟核心业务逻辑模块无关的功能抽离出来，这些 跟业务逻辑无关的功能通常包括日志统计、安全控制、异常处理等。把这些功能抽离出来之后， 再通过“动态织入”的方式掺入业务逻辑模块中。
+                这样做的好处首先是可以保持业务逻辑模块的纯净和高内聚性，其次是可以很方便地复用日志统计等功能模块。
+
+                在 Java语言中，可以通过 反射 和 动态代理机制 来实现 AOP技术。而在 JavaScript这种动态 语言中，AOP的实现更加简单，这是 JavaScript与生俱来的能力。 
+
+                通常，在 JavaScript中实现AOP，都是指把一个函数“动态织入”到另外一个函数之中，具 体的实现技术有很多，本节我们通过扩展 Function.prototype 来做到这一点。代码如下：
+                */
+
+                Function.prototype.before = function (beforefn) {
+                    var self = this;    // 保存原函数的引用     
+                    return function () {    // 返回包含了原函数和新函数的"代理"函数         
+                        beforefn.apply(this, arguments);     // 执行新函数，修正 this         
+                        return self.apply(this, arguments);    // 执行原函数     
+                    }
+                };
+
+                Function.prototype.after = function (afterfn) {
+                    var self = this;
+                    return function () {
+                        var ret = self.apply(this, arguments);
+                        afterfn.apply(this, arguments);
+                        return ret;
+                    }
+                };
+                var func = function () {
+                    console.log(2);
+                };
+                func = func.before(function () {
+                    console.log(1);
+                }).after(function () {
+                    console.log(3);
+                });
+                func();
+                //我们把负责打印数字 1和打印数字 3的两个函数通过 AOP的方式动态植入 func 函数。通过 执行上面的代码，我们看到控制台顺利地返回了执行结果 1、2、3。 
+            }
         }
     }
-
-
-
 }();
 
 
