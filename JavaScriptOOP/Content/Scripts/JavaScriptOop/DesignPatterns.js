d9f31f315ed42d665bec658475c026a560f85c26
yangpan <123456>
2016-07-10 21:11:23 +0800

    验证

--- JavaScriptOOP/Content/Scripts/JavaScriptOop/DesignPatterns.js
+++ JavaScriptOOP/Content/Scripts/JavaScriptOop/DesignPatterns.js
@@ -154,6 +154,173 @@ var designPatterns = function () {
             //return { awe: awe }
         };
 
+        //原始验证
+        var originalValidate = function () {
+            var registerForm = document.getElementById('registerForm');
+            registerForm.onsubmit = function() {
+                if (registerForm.userName.value === '') {
+                    alert(' 用户名不能为空');
+                    return false;
+                }
+                if (registerForm.password.value.length < 6) {
+                    alert(' 密码长度不能少于 6 位');
+                    return false;
+                }
+                if (!/(^1[3|5|8][0-9]{9}$)/.test(registerForm.phoneNumber.value)) {
+                    alert(' 手机号码格式不正确 ');
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        //策略模式重构验证(单一验证)
+        var strategyValidate = function () {
+
+            var strategies = {
+                isNonEmpty: function (value, errorMsg) { // 不为空
+                    if (value === '') {
+                        return errorMsg;
+                    }
+                },
+                minLength: function(value, length, errorMsg) { // 限制最小长度
+                    if (value.length < length) {
+                        return errorMsg;
+                    }
+                },
+                isMobile: function (value, errorMsg) { // 手机号码格式
+                    if (!/(^1[3|5|8][0-9]{9}$)/.test(value)) {
+                        return errorMsg;
+                    }
+                }
+            };
+
+            var Validator = function () {
+                this.cache = []; // 保存校验规则
+            };
+            Validator.prototype.add = function (dom, rule, errorMsg) {
+                var ary = rule.split(':'); // 把 strategy 和参数分开
+                this.cache.push(function () { // 把校验的步骤用空函数包装起来，并且放入 cache
+                    var strategy = ary.shift(); // 用户挑选的 strategy
+                    ary.unshift(dom.value); // 把 input 的 value 添加进参数列表
+                    ary.push(errorMsg); // 把 errorMsg 添加进参数列表
+                    return strategies[strategy].apply(dom, ary);
+                });
+            };
+            Validator.prototype.start = function() {
+                for (var i = 0, validatorFunc; validatorFunc === this.cache[i++];) {
+                    var msg = validatorFunc(); // 开始校验，并取得校验后的返回信息
+                    if (msg) { // 如果有确切的返回值，说明校验没有通过
+                        return msg;
+                    }
+                }
+            };
+
+            var validataFunc = function () {
+                var validator = new Validator(); // 创建一个 validator 对象
+                /***************添加一些校验规则 ****************/
+                validator.add(registerForm.userName, 'isNonEmpty', ' 用户名不能为空');
+                validator.add(registerForm.password, 'minLength:6', ' 密码长度不能少于 6 位');
+                validator.add(registerForm.phoneNumber, 'isMobile', ' 手机号码格式不正确 ');
+                var errorMsg = validator.start(); // 获得校验结果
+                return errorMsg; // 返回校验结果
+            };
+
+            var volidate = function () {
+                var registerForm = document.getElementById('registerForm');
+                registerForm.onsubmit = function () {
+                    var errorMsg = validataFunc(); // 如果 errorMsg 有确切的返回值，说明未通过校验
+                    if (errorMsg) {
+                        alert(errorMsg);
+                        return false; // 阻止表单提交
+                    }
+                }
+            }
+
+            return { volidate: volidate };
+        }
+
+        ////策略模式重构验证(多重验证)
+        var strategyManyValidate = function () {
+
+            var strategies = {
+                isNonEmpty: function (value, errorMsg) { // 不为空
+                    if (value === '') {
+                        return errorMsg;
+                    }
+                },
+                minLength: function (value, length, errorMsg) { // 限制最小长度
+                    if (value.length < length) {
+                        return errorMsg;
+                    }
+                },
+                isMobile: function (value, errorMsg) { // 手机号码格式
+                    if (!/(^1[3|5|8][0-9]{9}$)/.test(value)) {
+                        return errorMsg;
+                    }
+                }
+            };
+
+            var Validator = function () {
+                this.cache = [];
+            };
+            Validator.prototype.add = function(dom, rules) {
+                var self = this;
+                for (var i = 0, rule; rule = rules[i++];) {
+                    (function(rule) {
+                        var strategyAry = rule.strategy.split(':');
+                        var errorMsg = rule.errorMsg;
+                        self.cache.push(function() {
+                            var strategy = strategyAry.shift();
+                            strategyAry.unshift(dom.value);
+                            strategyAry.push(errorMsg);
+                            return strategies[strategy].apply(dom, strategyAry);
+                        });
+                    })(rule);
+                }
+            };
+            Validator.prototype.start = function() {
+                for (var i = 0, validatorFunc; validatorFunc = this.cache[i++];) {
+                    var errorMsg = validatorFunc();
+                    if (errorMsg) {
+                        return errorMsg;
+                    }
+                }
+            };
+
+            var validate = function () {
+                var registerForm = document.getElementById('registerForm');
+                var validataFunc = function() {
+                    var validator = new Validator();
+                    validator.add(registerForm.userName, [{
+                        strategy: 'isNonEmpty',
+                        errorMsg: ' 用户名不能为空'
+                    }, {
+                        strategy: 'minLength:6',
+                        errorMsg: ' 用户名长度不能小于 10 位'
+                    }]);
+                    validator.add(registerForm.password, [{
+                        strategy: 'minLength:6',
+                        errorMsg: ' 密码长度不能小于 6 位'
+                    }]);
+                    validator.add(registerForm.phoneNumber, [{
+                        strategy: 'isMobile',
+                        errorMsg: ' 手机号码格式不正确 '
+                    }]);
+                    var errorMsg = validator.start();
+                    return errorMsg;
+                };
+                registerForm.onsubmit = function() {
+                    var errorMsg = validataFunc();
+                    if (errorMsg) {
+                        alert(errorMsg);
+                        return false;
+                    }
+                }
+            }
+            return { validate: validate };
+
+        }
 
         return { example2: example2 };
     }
